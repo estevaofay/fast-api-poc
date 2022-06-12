826728aaff536c66c1e0cbffe28c08bc2484b412
Estêvão Francisco Fáy <estevao@getmentum.com>
2022-06-12 12:41:22 -0300

    Fix unit tests

--- app/test/test_main.py
+++ app/test/test_main.py
@@ -1,10 +1,13 @@
 from fastapi.testclient import TestClient
 
+from app.config import settings
 from app.main import app
 
 client = TestClient(app)
+settings.test_url = "https://testurl.com"
+
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == "Hello TikTok!"
+    assert response.json() == {'test_url': 'https://testurl.com'}
